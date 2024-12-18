e7a8b1ed28ad024dedd78c2d40e82199745d446e
anyong <andrew4may@gmail.com>
2024-12-19 00:09:00 +0900

    Feat: shopping cart CRUD function created

---
+++ src/main/java/shop/ninescent/cartItem/domain/Cart.java
@@ -0,0 +1,17 @@
+package shop.ninescent.cartItem.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+    @Column(nullable = false)
+    private Long userNo;
+    @Column(nullable = false)
+    private Integer totalCount;
+}
---
+++ src/main/java/shop/ninescent/cartItem/domain/CartItemDTO.java
@@ -0,0 +1,11 @@
+package shop.ninescent.cartItem.domain;
+
+import lombok.Data;
+
+@Data
+public class CartItemDTO {
+    private Long cartId;
+    private Long itemId;
+    private Integer quantity;
+    private String action;     // increase, decrease, set
+}
