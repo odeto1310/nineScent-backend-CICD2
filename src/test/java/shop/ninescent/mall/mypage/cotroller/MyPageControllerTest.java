904e9cbf83e26c4d41ff398546eec0da0a065265
Oh Ji Hyeon <dhwlgus0419@gmail.com>
2025-01-10 07:20:08 +0900

    BUILD

---
+++ src/test/java/shop/ninescent/mall/mypage/cotroller/MyPageControllerTest.java
@@ -0,0 +1,28 @@
+package shop.ninescent.mall.mypage.cotroller;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class MyPageControllerTest {
+
+    @Test
+    void addOrUpdateAddress() {
+    }
+
+    @Test
+    void getShippingStatus() {
+    }
+
+    @Test
+    void requestOrderChange() {
+    }
+
+    @Test
+    void writeReview() {
+    }
+
+    @Test
+    void getAllReviewsByUser() {
+    }
+}
