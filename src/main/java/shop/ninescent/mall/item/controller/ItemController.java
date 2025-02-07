a5c4574c7b762706dd436d24e0f2d95ee80dfc7f
ruedal <gpfls6008@naver.com>
2025-02-07 21:55:35 +0900

    Feat: AWS S3

--- src/main/java/shop/ninescent/mall/item/controller/ItemController.java
+++ src/main/java/shop/ninescent/mall/item/controller/ItemController.java
@@ -1,9 +1,14 @@
 package shop.ninescent.mall.item.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import shop.ninescent.mall.item.domain.Item;
 import shop.ninescent.mall.item.service.ItemService;
+import shop.ninescent.mall.item.service.S3Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -11,6 +16,24 @@ import java.util.List;
 public class ItemController {
 
     private final ItemService itemService;
+
+    @Autowired
+    private S3Service s3Service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("images") MultipartFile[] files) {
+        if (files.length > 15) {
+            return ResponseEntity.badRequest().body("Error: Cannot upload more than 15 files at a time.");
+        }
+        for (MultipartFile file : files) {
+            try {
+                s3Service.uploadFile(file);
+            } catch (IOException e) {
+                return ResponseEntity.internalServerError().body("File upload failed: " + e.getMessage());
+            }
+        }
+        return ResponseEntity.ok("Files uploaded successfully");
+    }
 
     public ItemController(ItemService itemService) {
         this.itemService = itemService;
