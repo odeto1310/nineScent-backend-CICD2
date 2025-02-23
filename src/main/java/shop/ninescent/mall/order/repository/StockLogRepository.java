12f8227b2dcb02e5e1c920fded00ed43f2a5afe6
anyong <andrew4may@gmail.com>
2025-02-24 01:41:53 +0900

    Feat: Mypage order history

--- src/main/java/shop/ninescent/mall/order/repository/StockLogRepository.java
+++ src/main/java/shop/ninescent/mall/order/repository/StockLogRepository.java
@@ -1,15 +1,15 @@
-package shop.ninescent.mall.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import shop.ninescent.mall.order.domain.StockLog;
-
-import java.util.Optional;
-
-@Repository
-public interface StockLogRepository extends JpaRepository<StockLog, Long> {
-    Optional<StockLog> findTopByItemItemIdAndChangeTypeOrderByCreatedAtDesc(Long itemId, String changeType);
-
-    Optional<StockLog> findByLogIdAndChangeType(Long logId, String changeType);
-
-}
+//package shop.ninescent.mall.order.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import shop.ninescent.mall.order.domain.StockLog;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface StockLogRepository extends JpaRepository<StockLog, Long> {
+//    Optional<StockLog> findTopByItemItemIdAndChangeTypeOrderByCreatedAtDesc(Long itemId, String changeType);
+//
+//    Optional<StockLog> findByLogIdAndChangeType(Long logId, String changeType);
+//
+//}
